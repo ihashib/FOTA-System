8329181b3080b7de9073edb34e8e45483cae34ee
ihashib <ihashib2@gmail.com>
2023-01-16 16:56:48 +0600

    inital project setup

---
+++ FOTA-Server/src/main/java/com/fota/fotaserver/FotaServerApplication.java
@@ -0,0 +1,13 @@
+package com.fota.fotaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotaServerApplication.class, args);
+	}
+
+}
---
+++ FOTA-Server/src/test/java/com/fota/fotaserver/FotaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fota.fotaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
